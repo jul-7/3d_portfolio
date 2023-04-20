0e0047eb4be4dcb855829801e9f22d41f9e95938
jul-7 <124777800+jul-7@users.noreply.github.com>
2023-04-20 14:56:38 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,5 @@
 import { BallCanvas } from "./canvas"
-import SectionWrapper from "../hoc/SectionWrapper"
+import { SectionWrapper } from "../hoc/SectionWrapper"
 import { technologies } from "../constants"
 
 const Tech = () => {
